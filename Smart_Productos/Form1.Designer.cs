2aab9a090ad76b4b19be478e53b13f3711d8b1af
Favelalvara01 <osielfavelaalv@gmail.com>
2026-03-12 09:16:20 -0600

    Agregar archivos de proyecto.

---
+++ Smart_Productos/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Smart_Productos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTV = new System.Windows.Forms.Button();
+            this.btnWatch = new System.Windows.Forms.Button();
+            this.btnPeakers = new System.Windows.Forms.Button();
+            this.btnLigth = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTV
+            // 
+            this.btnTV.BackColor = System.Drawing.Color.DarkGray;
+            this.btnTV.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTV.Location = new System.Drawing.Point(24, 96);
+            this.btnTV.Name = "btnTV";
+            this.btnTV.Size = new System.Drawing.Size(117, 30);
+            this.btnTV.TabIndex = 0;
+            this.btnTV.Text = "Smart Tv";
+            this.btnTV.UseVisualStyleBackColor = false;
+            this.btnTV.Click += new System.EventHandler(this.btnTV_Click);
+            // 
+            // btnWatch
+            // 
+            this.btnWatch.BackColor = System.Drawing.Color.DarkGray;
+            this.btnWatch.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWatch.Location = new System.Drawing.Point(24, 302);
+            this.btnWatch.Name = "btnWatch";
+            this.btnWatch.Size = new System.Drawing.Size(144, 36);
+            this.btnWatch.TabIndex = 1;
+            this.btnWatch.Text = "Smart Watch";
+            this.btnWatch.UseVisualStyleBackColor = false;
+            this.btnWatch.Click += new System.EventHandler(this.btnWatch_Click);
+            // 
+            // btnPeakers
+            // 
+            this.btnPeakers.BackColor = System.Drawing.Color.DarkGray;
+            this.btnPeakers.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPeakers.Location = new System.Drawing.Point(282, 96);
+            this.btnPeakers.Name = "btnPeakers";
+            this.btnPeakers.Size = new System.Drawing.Size(159, 38);
+            this.btnPeakers.TabIndex = 2;
+            this.btnPeakers.Text = "Smart Peakers";
+            this.btnPeakers.UseVisualStyleBackColor = false;
+            this.btnPeakers.Click += new System.EventHandler(this.btnPeakers_Click);
+            // 
+            // btnLigth
+            // 
+            this.btnLigth.BackColor = System.Drawing.Color.DarkGray;
+            this.btnLigth.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLigth.Location = new System.Drawing.Point(282, 302);
+            this.btnLigth.Name = "btnLigth";
+            this.btnLigth.Size = new System.Drawing.Size(135, 36);
+            this.btnLigth.TabIndex = 3;
+            this.btnLigth.Text = "Smart Light";
+            this.btnLigth.UseVisualStyleBackColor = false;
+            this.btnLigth.Click += new System.EventHandler(this.btnLigth_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(277, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Smart Productos";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(564, 96);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(353, 148);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSalir.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(741, 325);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(124, 46);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnLimpiar.Font = new System.Drawing.Font("Bauhaus 93", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(564, 325);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(123, 46);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox4.Image = global::Smart_Productos.Properties.Resources.Untitled__1_;
+            this.pictureBox4.Location = new System.Drawing.Point(24, 132);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(159, 112);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox3.Image = global::Smart_Productos.Properties.Resources.Asistente;
+            this.pictureBox3.Location = new System.Drawing.Point(234, 132);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(159, 112);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Smart_Productos.Properties.Resources.Untitled;
+            this.pictureBox2.Location = new System.Drawing.Point(282, 344);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(159, 119);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Smart_Productos.Properties.Resources.Adobe_Express___file1;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 344);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 119);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(967, 491);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLigth);
+            this.Controls.Add(this.btnPeakers);
+            this.Controls.Add(this.btnWatch);
+            this.Controls.Add(this.btnTV);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTV;
+        private System.Windows.Forms.Button btnWatch;
+        private System.Windows.Forms.Button btnPeakers;
+        private System.Windows.Forms.Button btnLigth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Smart_Productos/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_Productos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTV_Click(object sender, EventArgs e)
+        {
+            Productos tv = new Productos();
+            tv.Marca = "Samsung";
+            tv.Resolucion = "4K";
+            tv.Tamaño_Pantalla = 55;
+            tv.Precio = 1200;
+
+            txtResultado.Text = "** Smart TV **\r\n" +
+                                "   Marca:    " + tv.Marca + "\r\n" +
+                                "   Resol:    " + tv.Resolucion + "\r\n" +
+                                "   Pantalla: " + tv.Tamaño_Pantalla + "\"\r\n" +
+                                "   Precio:   $" + tv.Precio + "\r\n\r\n" +
+                                tv.Cambiar_Canal() + "\r\n" +
+                                tv.Abrir_App();
+        }
+
+        private void btnPeakers_Click(object sender, EventArgs e)
+        {
+            Productos speaker = new Productos();
+            speaker.Marca = "Bose";
+            speaker.Asistente_voz = "Alexa";
+            speaker.Potencia_sonido = 100;
+
+            txtResultado.Text = "   Smart Speaker **\r\n" +
+                                "   Marca:     " + speaker.Marca + "\r\n" +
+                                "   Asistente: " + speaker.Asistente_voz + "\r\n" +
+                                "   Potencia:  " + speaker.Potencia_sonido + "W\r\n\r\n" +
+                                speaker.Reproducir_Musica() + "\r\n" +
+                                speaker.Detener_Audio();
+        }
+        private void btnWatch_Click(object sender, EventArgs e)
+        {
+            Productos watch = new Productos();
+            watch.Marca = "Apple";
+            watch.Precio = 400;
+            watch.Sensor_Ritmo = true;
+            watch.Autonomia_Bateria = 18;
+
+            txtResultado.Text = "** Smart Watch **\r\n" +
+                                "   Marca:    " + watch.Marca + "\r\n" +
+                                "   Precio:   $" + watch.Precio + "\r\n" +
+                                "   Batería:  " + watch.Autonomia_Bateria + "h\r\n\r\n" +
+                                watch.Monitorear_Pasos() + "\r\n" +
+                                watch.Medir_Ritmo();
+        }
+
+        private void btnLigth_Click(object sender, EventArgs e)
+        {
+            Productos light = new Productos();
+            light.Marca = "Philips";
+            light.Tipo_Conector = "USB-C";
+            light.Color_Rgb = true;
+            light.Precio = 50;
+
+            txtResultado.Text = "** Smart Light **\r\n" +
+                                "   Marca:     " + light.Marca + "\r\n" +
+                                "   Conector:  " + light.Tipo_Conector + "\r\n" +
+                                "   Precio:    $" + light.Precio + "\r\n\r\n" +
+                                light.Conectar_Bluetooth() + "\r\n" +
+                                light.Regular_Intensidad();
+        }
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
